e2d6fd5e7e2ff02575bcf79937482d41d77bd701
Ilan <apps@calaverse.com>
2017-01-11 21:18:48 +0000

    first commit

---
+++ Ex_Files_React_EssT/Ch02/02_07/start/webpack.config.js
@@ -0,0 +1,48 @@
+var webpack = require("webpack");
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		path: "dist/assets",
+		filename: "bundle.js",
+		publicPath: "assets"
+	},
+	devServer: {
+		inline: true,
+		contentBase: './dist',
+		port: 8080,
+    host: '0.0.0.0'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loader: ["babel-loader"],
+				query: {
+					presets: ["latest", "stage-0", "react"]
+				}
+			},
+			{
+				test: /\.json$/,
+				exclude: /(node_modules)/,
+				loader: "json-loader"
+			},
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!autoprefixer-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
+      }
+		]
+	}
+}
+
+
+
+
+
+
+
